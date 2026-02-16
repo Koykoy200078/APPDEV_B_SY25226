ac3d8b4e17fdded28d371b03ff76abe9ed187c13
Koykoy200078 <franc200078@gmail.com>
2026-02-16 09:32:13 +0800

    feat: CustomTextInput and CustomButton

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,28 +1,23 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { useEffect } from 'react';
+import { Platform, StatusBar, useColorScheme } from 'react-native';
 
-// screens
-import HomeScreen from '../screens/HomeScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-
-// utils -> INDEX
-import { ROUTES } from '../utils';
-
-const Stack = createStackNavigator();
-
-const MainNavigation = () => {
-  return (
-    <Stack.Navigator initialRouteName={ROUTES.HOME}>
-      <Stack.Screen name={ROUTES.HOME} component={HomeScreen} />
-      <Stack.Screen name={ROUTES.PROFILE} component={ProfileScreen} />
-    </Stack.Navigator>
-  );
-};
+import AuthNav from './AuthNav';
 
 export default () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      StatusBar.setBackgroundColor('#000000', true);
+    }
+
+    StatusBar.setBarStyle('dark-content', true);
+  }, [isDarkMode]);
+
   return (
     <NavigationContainer>
-      <MainNavigation />
+      <AuthNav />
     </NavigationContainer>
   );
 };
