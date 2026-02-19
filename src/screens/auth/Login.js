ad9b1a1a1f332ec64c88194e846e1005d1f9afb9
Koykoy200078 <franc200078@gmail.com>
2026-02-19 10:00:08 +0800

    chore: add app icon, add register screen and remove headershown in auth

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,16 +1,17 @@
 import { useState } from 'react';
-import { Alert, Text, View } from 'react-native';
+import { Alert, Text, TouchableOpacity, View } from 'react-native';
 
+import { useNavigation } from '@react-navigation/native';
 import CustomButton from '../../components/CustomButton';
 import CustomTextInput from '../../components/CustomTextInput';
-import { useNavigation } from '@react-navigation/native';
+import { ROUTES } from '../../utils';
 
 const Login = () => {
   // GETTER //SETTER
   const [emailAdd, setEmailAdd] = useState('');
   const [password, setPassword] = useState('');
 
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   //   useEffect(() => {}, [emailAdd, password]);
 
@@ -19,47 +20,72 @@ const Login = () => {
       <Text style={{ color: 'black' }}>{emailAdd}</Text>
       <Text style={{ color: 'black' }}>{password}</Text>
 
-      <CustomTextInput
-        label={'Email Address'}
-        placeholder={'Email Address'}
-        value={val => setEmailAdd(val)}
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <CustomTextInput
+          label={'Email Address'}
+          placeholder={'Email Address'}
+          value={val => setEmailAdd(val)}
+          containerStyle={{
+            width: '100%',
+            marginBottom: 15,
+          }}
+          labelStyle={{
+            fontSize: 20,
+            fontWeight: '500',
+          }}
+          textStyle={{
+            fontSize: 20,
+          }}
+        />
+
+        <CustomTextInput
+          label={'Password'}
+          placeholder={'Password'}
+          value={val => setPassword(val)}
+          containerStyle={{
+            width: '100%',
+          }}
+          labelStyle={{
+            fontSize: 20,
+            fontWeight: '500',
+          }}
+          textStyle={{
+            fontSize: 20,
+          }}
+        />
+      </View>
+
+      <CustomButton
+        label={'LOGIN'}
         containerStyle={{
-          width: '100%',
-        }}
-        labelStyle={{
-          fontSize: 20,
-          fontWeight: '500',
+          marginVertical: 20,
+          width: '80%',
+          backgroundColor: 'blue',
+          borderRadius: 10,
         }}
         textStyle={{
+          color: '#ffffff',
+          textAlign: 'center',
+          fontWeight: '800',
           fontSize: 20,
+        }}
+        onPress={() => {
+          if (emailAdd === '' && password === '') {
+            Alert.alert('Incorrect Credentials', 'Please try again!');
+            return;
+          }
         }}
       />
 
-      <CustomTextInput
-        label={'Password'}
-        placeholder={'Password'}
-        value={val => setPassword(val)}
-        containerStyle={{
-          width: '100%',
-        }}
-        labelStyle={{
-          fontSize: 20,
-          fontWeight: '500',
-        }}
-        textStyle={{
-          fontSize: 20,
-        }}
-      />
-
-      <CustomButton label={'LOGIN'} onPress={() => {
-        if (emailAdd) Alert.alert("Incorrect Credentials", "Please try again!");
-
-
-        
-
-
-
-      }} />
+      <View style={{ flexDirection: 'row' }}>
+        <Text>Not register yet?</Text>
+        <TouchableOpacity
+          style={{ marginLeft: 5 }}
+          onPress={() => navigation.navigate(ROUTES.REGISTER)}
+        >
+          <Text style={{ color: 'red', fontWeight: '800' }}>Register</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
